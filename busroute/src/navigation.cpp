d935322afb999664665e7e47826743094367b064
kajMork <kmark19@student.aau.dk>
2019-11-07 14:07:17 +0100

    TroubleShooting navigation and getsensor files

--- busroute/src/navigation.cpp
+++ busroute/src/navigation.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-#include "getSensor.h" 
+#include "getSensor.h"
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
@@ -27,9 +27,40 @@ int main(int argc, char** argv){
   goal.target_pose.pose.position.x = 1.0;
   goal.target_pose.pose.orientation.w = 1.0;
 
+
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
+ if (sAct.bumper_pressed_center || sAct.bumper_pressed_left || sAct.bumper_pressed_right)
+     {    
+          for (int i=0; i<5; i++)
+          {
+            sAct.vel.linear.x = -0.0;
+            sAct.vel.angular.z = 0.0;
+            sAct.cmd_vel_pub.publish(sAct.vel);
+            ROS_INFO_STREAM("BACKWARDS" << i);
+            ros::Duration(0.1).sleep();
+          }
+          for(int i=0; i<45; i++)
+          {
+            sAct.vel.linear.x = 0.0;
+            sAct.vel.angular.z = 0.0;
+            sAct.cmd_vel_pub.publish(sAct.vel);
+            ROS_INFO_STREAM("ROTATING " << i);
+            ros::Duration(0.1).sleep();
+          }
+          sAct.bumper_pressed_center = false;
+          sAct.bumper_pressed_left = false;
+          sAct.bumper_pressed_right = false;
+          //ms.vel.angular.z = 4.0;
+          
+     }
 
+  if (sAct.wheeldropped || sAct.wheeldropped_left || sAct.wheeldropped_right)
+  {
+    ac.cancelGoal();
+    ROS_INFO("Turtlebot is being lifted or tilted! Goal canceled");
+
+  }
   ac.waitForResult();
 
   //If we receive result and its succeded:
