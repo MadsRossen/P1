77987b9e3a808f6263bd4e24d37a8e4e7104a0d3
kajMork <kmark19@student.aau.dk>
2019-12-06 14:53:53 +0100

    Update navigation and parameters

--- busroute/src/navigation.cpp
+++ busroute/src/navigation.cpp
@@ -58,12 +58,10 @@ int main(int argc, char **argv){
   
   //LineDetect det;
   //ImageConverter imgcon;
- //tell the clients that we want to spin a thread by default
   
   ros::NodeHandle nh;
   ros::ServiceClient GetMapClient = nh.serviceClient<nav_msgs::GetMap>("dynamic_map");
   nav_msgs::GetMap srv_map;
-  explore::Explore explore;
   ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
   visualization_msgs::Marker marker;
 
@@ -75,53 +73,50 @@ int main(int argc, char **argv){
   // Assign the docking initial state:
   //actionlib::SimpleClientGoalState dock_state = actionlib::SimpleClientGoalState::LOST;
 
-
-//This while loop should not be necessary, but it need to be tested
-
-
-
 float x_InitialPose = sAct.x_currentPose;
 float y_InitialPose = sAct.y_currentPose;
+ros::Rate r(25);
 while (runningexp)
 {
+  explore::Explore explore;
   if (!explore.stopped)
   {   
     runningexp = false;
     runningnav = true;
     running = true;
+    explore.~Explore();
   }
-  ros::spinOnce();
+  ros::spin();
+  
 }
 //runningnav = true;
 //running = true;
 while (runningnav)
 {
-if (!sAct.bumper_pressed_center)
-{
-  ROS_INFO_STREAM("BUMPER NOT DETECTED");
-}
-  ROS_INFO_STREAM("TESTTEST");
-    explore.~Explore();
+  //tell the clients that we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
+  //Wait until the server has come up
     while(!ac.waitForServer(ros::Duration(5.0))){
       ROS_INFO_STREAM("Waiting for the move_base action server to come up");
     }
   
-
+//Reading the costmap and getting size of the mapped area.
   ROS_INFO_STREAM("RUNNING LOOP");
      if (GetMapClient.call(srv_map))
       {
         ROS_INFO("Map service called successfully");
+        //Get the size of the costmap
         map_size_i_= srv_map.response.map.info.width -1;
         map_size_j_ = srv_map.response.map.info.height -1;
         //x_pos = srv_map.response.map.info.origin.position.x;
         //y_pos = srv_map.response.map.info.origin.position.y;
         int sorted_costmap[map_size_i_][map_size_j_];
         int unsorted_costmap[map_size_i_][map_size_j_];
+        //Output costmap size
         std::cout << "Total Map_size_i_"<<"="<<map_size_i_<< std::endl;
         std::cout << "Total Map_size_j_"<<"="<<map_size_j_<< std::endl;
 
-        //for (j=map_size_j_; j>=0; j-- )
+        //Running the loop to get costmap data;
         for (j=0; j<=map_size_j_; j++ )
           {
                for (i=0; i<=map_size_i_; i++ )
@@ -130,7 +125,7 @@ if (!sAct.bumper_pressed_center)
                     {
                       x_first_ob = i;
                       y_first_ob = j;
-                      //map_size_j_ - j
+                      
                       firstobstacle = false;
                     }
                     if (srv_map.response.map.data[u] >= 0)
@@ -140,9 +135,7 @@ if (!sAct.bumper_pressed_center)
                       sendEnter = true;
                       icount ++;
                     }
-                    u++;
-                    
-                    
+                    u++;      
                }
                if (sendEnter)
                {
@@ -154,8 +147,7 @@ if (!sAct.bumper_pressed_center)
                     countbefore = icount;
                  }
                  icount = 0;
-               }
-                 
+               }             
           }
       std::cout << "Used Map size"<<"="<<"y:"<<jcount <<","<<"x:"<<countbefore<< std::endl;
       startGetMap = false;
@@ -165,207 +157,165 @@ if (!sAct.bumper_pressed_center)
         ROS_ERROR("Failed to call service GetMap");
         return 1;
       }
-
+//Starting the pathplanner:
+  std::cout << "First obstacle X"<<"="<<x_first_ob<< std::endl;
+  std::cout << "First obstacle Y"<<"="<<y_first_ob<< std::endl;
   move_base_msgs::MoveBaseGoal goal;
-  goal_y = 1.0;
-  goal_x = 1.0;
-  //we'll send a goal to the robot to move 1 meter forward
-  //We need to figure out what the frame_id is, base_link, /map or /odom ??
-  goal.target_pose.header.frame_id = "/map";
-  goal.target_pose.header.stamp = ros::Time::now();
 
-  //goal.target_pose.pose.position.x = 0.5;
-  //goal.target_pose.pose.position.y = 0.5;
-  //goal.target_pose.pose.orientation.w = 1.0;
+  map_res = srv_map.response.map.info.resolution;
+
+  //Variables for the first obstacle detected from lower left corner of costmap.
+  double x_firstObst_pos = x_first_ob * map_res + srv_map.response.map.info.origin.position.x;
+  std::cout << "x_firstIbst_pos"<<"="<<x_firstObst_pos<< std::endl;
+  double y_firstObst_pos = y_first_ob * map_res + srv_map.response.map.info.origin.position.y;
+  std::cout << "y_firstObst_pos"<<"="<<y_firstObst_pos<< std::endl;
+
+  //Variables for the mapsize of the costmap with mapped data converted to meter.
+  double y_mappedsize = jcount * map_res;
+  double x_mappedsize = countbefore * map_res;
+
+  //Variables for the Origin of the costmap (0,0) (Lower left corner)
   double x_origin = srv_map.response.map.info.origin.position.x;
-  double y_origin = srv_map.response.map.info.origin.position.y;
   std::cout << "x_origin"<<"="<<x_origin<< std::endl;
+  double y_origin = srv_map.response.map.info.origin.position.y;
   std::cout << "y_origin"<<"="<<y_origin<< std::endl;
-  double x_goal_pos = x_first_ob * map_res + srv_map.response.map.info.origin.position.x;
-  double y_goal_pos = y_first_ob * map_res + srv_map.response.map.info.origin.position.y;
-    if (GetMapClient.call(srv_map))
+
+  //Bool for running the pathplanner loop.
+  bool runPathPlanner = true;
+  //Variables for the size of the mapped costmap with a safety margin for the turtlebot.
+  double X_MAX = x_mappedsize - 0.5; 
+  double Y_MIN = y_firstObst_pos + 0.5;
+  //Variable for the Right upper corner with a safety margin for the turtlebot.
+  double r_U_C = y_mappedsize + y_firstObst_pos - 0.5;
+
+  double jj = r_U_C;
+  std::cout << "r_U_C"<<"="<<x_InitialPose<< std::endl;
+  double ii = 0.5;
+  int turn = 1;
+  //Bool for running goalState loop.
+  bool goalreached = false; 
+
+  std::cout << "Turtlebot pos_x"<<"="<<x_InitialPose<< std::endl;
+  std::cout << "Turtlebot pos_y"<<"="<<y_InitialPose<< std::endl;
+  //Starting the pathplanner loop.
+  while(runPathPlanner)
+  {
+    //Sending Goal;
+    goal.target_pose.header.frame_id = "/map";
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = ii;
+    goal.target_pose.pose.position.y = jj;
+    goal.target_pose.pose.orientation.w = 1.0;
+    ROS_INFO_STREAM("Sending goal");
+    goalreached = false;
+    ac.sendGoal(goal);
+    std::cout << "Turtlebot goal i"<<"="<<ii<< std::endl;
+    std::cout << "Turtlebot goal j"<<"="<<jj<< std::endl;
+    std::cout << "X_MAX"<<"="<<X_MAX<< std::endl;
+    std::cout << "Y_MIN"<<"="<<Y_MIN<< std::endl;
+    std::cout << "Turn"<<"="<<turn<< std::endl;
+    //Sending marker for current goal
+    marker.header.frame_id = "/map";
+    marker.header.stamp = ros::Time();
+    marker.ns = "Goal objective";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = ii;
+    marker.pose.position.y = jj;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 0.3;
+    marker.scale.y = 0.3;
+    marker.scale.z = 0.3;
+    marker.color.a = 1.0; // Don't forget to set the alpha!
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+    vis_pub.publish( marker );
+    while (!goalreached)
+    {
+      //If turtlebot is being lifted = abort the goal.
+      if (sAct.wheeldropped)
       {
-        map_res = srv_map.response.map.info.resolution;
-        goal.target_pose.pose.position.x = 0.5;
-        goal.target_pose.pose.position.y = 0.5;
-        goal.target_pose.pose.orientation.w = 1.0;
-      }
-    else
-      {
-        ROS_ERROR("Failed to call service GetMap");
-        return 1;
+        sc.playWave("/home/ubu/ws/src/P1/busroute/sounds/Reee.wav", 1.0);
+        ac.cancelGoal(); 
+        ROS_INFO("Turtlebot is being lifted or tilted! Goal canceled. Heading back to docking station");
+        sAct.wheeldropped = false;
       }
   
-std::cout << "First obstacle X"<<"="<<x_first_ob<< std::endl;
-std::cout << "First obstacle Y"<<"="<<y_first_ob<< std::endl;
-std::cout << "First obstacle X * mapres"<<"="<<x_goal_pos<< std::endl;
-std::cout << "First obstacle Y * mapres"<<"="<<y_goal_pos<< std::endl;
-std::cout << "Turtlebot pos_x"<<"="<<x_InitialPose<< std::endl;
-std::cout << "Turtlebot pos_y"<<"="<<y_InitialPose<< std::endl;
+      //If we receive result and its succeded:
+      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      {
+        ROS_INFO("Reached Goal destination!");
+        goalreached = true;
+        if (jj <= Y_MIN)
+        {
+          if (ii >= X_MAX)
+          {
+            runPathPlanner = false; 
+            runningnav = false; 
+          }
+          else
+          {
+            ii = X_MAX;
+          } 
+        }
+        else 
+        {
+          if (ii >= X_MAX)
+          {
+            if (turn = 1)
+            {
+              ii = X_MAX;
+              jj = r_U_C - 0.5;
+              turn = 2;
+            }
+            else
+            {
+              ii = 0.5;
+              turn = 1;
+            }
+          }
+          else
+          {
+            if (turn = 1)
+            {
+              ii = 0.5;
+              jj = r_U_C -0.5;
+              turn = 2;
+            }
+            else
+            {
+              ii = X_MAX;
+              turn = 1;
+            }
+          }
+        }
+      } 
+      //if something went wrong and it did not succed:
+      if(ac.getState() == actionlib::SimpleClientGoalState::ABORTED)
+      { 
+        ROS_INFO("The base failed to archive the goal");
+        ac.cancelAllGoals();
+        ac.sendGoal(goal);
+        vis_pub.publish( marker );
+      }
+    ros::spinOnce();
+    r.sleep();
+    }
+  
+    //Algorithm for defining new goal in the mapped area. 
 
-marker.header.frame_id = "/map";
-marker.header.stamp = ros::Time();
-marker.ns = "FirstObstacle";
-marker.id = 0;
-marker.type = visualization_msgs::Marker::SPHERE;
-marker.action = visualization_msgs::Marker::ADD;
-marker.pose.position.x = x_first_ob * map_res + srv_map.response.map.info.origin.position.x;
-marker.pose.position.y = y_first_ob * map_res + srv_map.response.map.info.origin.position.y;
-marker.pose.position.z = 0;
-marker.pose.orientation.x = 0.0;
-marker.pose.orientation.y = 0.0;
-marker.pose.orientation.z = 0.0;
-marker.pose.orientation.w = 1.0;
-marker.scale.x = 0.3;
-marker.scale.y = 0.3;
-marker.scale.z = 0.3;
-marker.color.a = 1.0; // Don't forget to set the alpha!
-marker.color.r = 0.0;
-marker.color.g = 1.0;
-marker.color.b = 0.0;
-vis_pub.publish( marker );
-
-marker.header.frame_id = "/map";
-marker.header.stamp = ros::Time();
-marker.ns = "map origin";
-marker.id = 0;
-marker.type = visualization_msgs::Marker::SPHERE;
-marker.action = visualization_msgs::Marker::ADD;
-marker.pose.position.x = 0.5;
-marker.pose.position.y = 0.5;
-marker.pose.position.z = 0;
-marker.pose.orientation.x = 0.0;
-marker.pose.orientation.y = 0.0;
-marker.pose.orientation.z = 0.0;
-marker.pose.orientation.w = 1.0;
-marker.scale.x = 0.3;
-marker.scale.y = 0.3;
-marker.scale.z = 0.3;
-marker.color.a = 1.0; // Don't forget to set the alpha!
-marker.color.r = 1.0;
-marker.color.g = 0.0;
-marker.color.b = 0.0;
-vis_pub.publish( marker );
-
-marker.header.frame_id = "/map";
-marker.header.stamp = ros::Time();
-marker.ns = "costmap cell left corner";
-marker.id = 0;
-marker.type = visualization_msgs::Marker::SPHERE;
-marker.action = visualization_msgs::Marker::ADD;
-marker.pose.position.x = map_res + srv_map.response.map.info.origin.position.x;
-marker.pose.position.y = map_res + srv_map.response.map.info.origin.position.y;
-marker.pose.position.z = 0;
-marker.pose.orientation.x = 0.0;
-marker.pose.orientation.y = 0.0;
-marker.pose.orientation.z = 0.0;
-marker.pose.orientation.w = 1.0;
-marker.scale.x = 0.3;
-marker.scale.y = 0.3;
-marker.scale.z = 0.3;
-marker.color.a = 1.0; // Don't forget to set the alpha!
-marker.color.r = 0.0;
-marker.color.g = 0.0;
-marker.color.b = 1.0;
-vis_pub.publish( marker );
-
-marker.header.frame_id = "/map";
-marker.header.stamp = ros::Time();
-marker.ns = "Robot InitialPose";
-marker.id = 0;
-marker.type = visualization_msgs::Marker::CUBE;
-marker.action = visualization_msgs::Marker::ADD;
-marker.pose.position.x = x_InitialPose;
-marker.pose.position.y = y_InitialPose;
-marker.pose.position.z = 0;
-marker.pose.orientation.x = 0.0;
-marker.pose.orientation.y = 0.0;
-marker.pose.orientation.z = 0.0;
-marker.pose.orientation.w = 1.0;
-marker.scale.x = 0.3;
-marker.scale.y = 0.3;
-marker.scale.z = 0.3;
-marker.color.a = 1.0; // Don't forget to set the alpha!
-marker.color.r = 0.0;
-marker.color.g = 0.0;
-marker.color.b = 1.0;
-vis_pub.publish( marker );
-
+  ros::spinOnce();
+  r.sleep();
+  }
   //https://answers.ros.org/question/197046/sending-map-co-ordinates-as-goal-to-move_base/ 
-  ROS_INFO_STREAM("Sending goal");
-  ac.sendGoal(goal);
-  while (running)
-{
-  
-  /*if (!det.img.empty()) {
-            // Perform image processing
-            det.img_filt = det.Gauss(det.img);
-            det.colorthresh(det.img_filt);
-            }*/
-  
-//If bumpers is pressed, we want to cancel goal, and get away from obstacle
-//Where we then sends the goal again.
- if (sAct.bumper_pressed_center || sAct.bumper_pressed_left || sAct.bumper_pressed_right )
-     {
-       ac.cancelGoal();    
-       ROS_INFO_STREAM("BUMPER DETECTED");
-          for (int i=0; i<5; i++)
-          {
-            sAct.vel.linear.x = -1.0;
-            sAct.vel.angular.z = 0.0;
-            sAct.cmd_vel_pub.publish(sAct.vel);
-            ROS_INFO_STREAM("BACKWARDS" << i);
-            ros::Duration(0.1).sleep();
-          }
-          for(int i=0; i<45; i++)
-          {
-            sAct.vel.linear.x = 0.0;
-            sAct.vel.angular.z = 1.0;
-            sAct.cmd_vel_pub.publish(sAct.vel);
-            ROS_INFO_STREAM("ROTATING " << i);
-            ros::Duration(0.1).sleep();
-          }
-          sAct.bumper_pressed_center = false;
-          sAct.bumper_pressed_left = false;
-          sAct.bumper_pressed_right = false;
-          //ms.vel.angular.z = 4.0;
-          ac.sendGoal(goal);
-     }
-//If turtlebot is tilted we want to cancel goal and send warning sound.
-  if (sAct.wheeldropped)
-  {
-    sc.playWave("/home/ubu/ws/src/P1/busroute/sounds/Reee.wav", 1.0);
-    ac.cancelGoal(); 
-    ROS_INFO("Turtlebot is being lifted or tilted! Goal canceled. Heading back to docking station");
-    sAct.wheeldropped = false;
-  }
-  
-  //ac.waitForResult();
-  //If we receive result and its succeded:
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  {
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-    running = false;
-    runningnav = false;
-    startGetMap = true;
-    
-  } 
-  //if something went wrong and it did not succed:
-  if(ac.getState() == actionlib::SimpleClientGoalState::ABORTED)
-  {
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
-    running = false;  
-    runningnav = false;
-    startGetMap = true;
-  }
-
- if (startGetMap)
-  {
-    
-  }
-ros::spinOnce(); 
-}
 ros::spinOnce();
+r.sleep();
 }
 
   ROS_INFO("DONE MAIN TASK");
